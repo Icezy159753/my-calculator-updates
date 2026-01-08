ff4080b955c746996c84761b54d7e287413b0f8c
songklod <songklod@rdm.com>
2026-01-08 11:20:49 +0700

    Bump version to 1.1.31

--- Main_Program.py
+++ Main_Program.py
@@ -64,7 +64,7 @@ UPDATE_HISTORY_URL = "https://dp1234.lovable.app/"
 PROGRAM_SUBFOLDER = "All_Programs"
 ICON_FOLDER = "Icon"
 # --- ข้อมูลโปรแกรมและ GitHub (สำคัญมาก: ต้องเปลี่ยนเป็นของคุณ) ---
-CURRENT_VERSION = "1.1.30"
+CURRENT_VERSION = "1.1.31"
 REPO_OWNER = "Icezy159753"  # << เปลี่ยนเป็นชื่อ Username ของคุณ
 REPO_NAME = "my-calculator-updates"    # << เปลี่ยนเป็นชื่อ Repository ของคุณ
 
